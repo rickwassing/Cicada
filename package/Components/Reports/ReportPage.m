1730884aa0d839c57e2a74a8d25be73e03734cea
Rick Wassing <rickwassing@gmail.com>
2025-11-11 15:00:01 +1100

    Initial report tab

---
+++ package/Components/Reports/ReportPage.m
@@ -0,0 +1,158 @@
+% REPORTPAGE
+% A single page component for the report
+
+% Authors:
+%   Rick Wassing, Woolcock Institute of Medical Research, Sydney, Australia
+%
+% History:
+%   Created 2025-11-11, Rick Wassing
+
+% Cicada (C) 2023 by Rick Wassing is licensed under
+% Attribution-NonCommercial-ShareAlike 4.0 International
+% This license requires that reusers give credit to the creator. It allows
+% reusers to distribute, remix, adapt, and build upon the material in any
+% medium or format, for noncommercial purposes only. If others modify or
+% adapt the material, they must license the modified material under
+% identical terms.
+
+classdef ReportPage < CicadaComponentContainer
+    % *********************************************************************
+    % PROPERTIES
+    properties
+        PageNum = 1;
+        HeaderHeight = 72;
+        FooterHeight = 72;
+        Margin = 36;  % Page margin in pixels
+    end
+    properties (Access = public, Transient, NonCopyable)
+        Panel matlab.ui.container.Panel
+        GridLayout matlab.ui.container.GridLayout
+        HeaderPanel matlab.ui.container.Panel
+        HeaderGridLayout matlab.ui.container.GridLayout
+        BodyPanel matlab.ui.container.Panel
+        BodyGridLayout matlab.ui.container.GridLayout
+        FooterPanel matlab.ui.container.Panel
+        FooterGridLayout matlab.ui.container.GridLayout
+        % Sub-components
+        Components = struct();
+    end
+    % *********************************************************************
+    % METHODS
+    methods (Access = protected)
+        % =================================================================
+        function setup(Obj)
+            % -------------------------------------------------------------
+            % Create the main panel (represents the page)
+            % -------------------------------------------------------------
+            Obj.Tag = 'ReportPage';
+            Obj.Panel = uipanel(Obj, ...
+                'Units', 'normalized', ...
+                'BorderColor', [0.651, 0.651, 0.651], ...
+                'HighlightColor', [0.651, 0.651, 0.651], ...
+                'BorderType', 'none', ...
+                'BackgroundColor', [1, 1, 1], ...
+                'Position', [0, 0, 1, 1]);
+            % -------------------------------------------------------------
+            % Create the main grid layout (3 rows: header/body/footer)
+            % -------------------------------------------------------------
+            Obj.GridLayout = uigridlayout(Obj.Panel, ...
+                'ColumnWidth', {'1x'}, ...
+                'RowHeight', {Obj.HeaderHeight, '1x', Obj.FooterHeight}, ...
+                'ColumnSpacing', 0, ...
+                'RowSpacing', 0, ...
+                'Padding', [Obj.Margin, Obj.Margin, Obj.Margin, Obj.Margin], ...
+                'BackgroundColor', [1, 1, 1]);
+            % -------------------------------------------------------------
+            % Create the header panel
+            % -------------------------------------------------------------
+            Obj.HeaderPanel = uipanel(Obj.GridLayout, ...
+                'BorderColor', [1, 1, 1], ...
+                'HighlightColor', [1, 1, 1], ...
+                'BackgroundColor', [1, 1, 1]);
+            Obj.HeaderPanel.Layout.Row = 1;
+            Obj.HeaderPanel.Layout.Column = 1;
+            
+            Obj.HeaderGridLayout = uigridlayout(Obj.HeaderPanel, ...
+                'ColumnWidth', {'2x', '1x'}, ...
+                'RowHeight', {'1x'}, ...
+                'Padding', [0, 0, 0, 0], ...
+                'BackgroundColor', [1, 1, 1]);
+            % -------------------------------------------------------------
+            % Create the body panel
+            % -------------------------------------------------------------
+            Obj.BodyPanel = uipanel(Obj.GridLayout, ...
+                'BorderColor', [1, 1, 1], ...
+                'HighlightColor', [1, 1, 1], ...
+                'BackgroundColor', [1, 1, 1]);
+            Obj.BodyPanel.Layout.Row = 2;
+            Obj.BodyPanel.Layout.Column = 1;
+            
+            Obj.BodyGridLayout = uigridlayout(Obj.BodyPanel, ...
+                'ColumnWidth', {'1x'}, ...
+                'RowHeight', {'1x'}, ...
+                'Padding', [0, 0, 0, 0], ...
+                'BackgroundColor', [1, 1, 1]);
+            % -------------------------------------------------------------
+            % Create the footer panel
+            % -------------------------------------------------------------
+            Obj.FooterPanel = uipanel(Obj.GridLayout, ...
+                'BorderColor', [1, 1, 1], ...
+                'HighlightColor', [1, 1, 1], ...
+                'BackgroundColor', [1, 1, 1]);
+            Obj.FooterPanel.Layout.Row = 3;
+            Obj.FooterPanel.Layout.Column = 1;
+            
+            Obj.FooterGridLayout = uigridlayout(Obj.FooterPanel, ...
+                'ColumnWidth', {'1x'}, ...
+                'RowHeight', {50}, ...
+                'Padding', [72, 36, 72, 4], ...
+                'BackgroundColor', [1, 1, 1]);
+            % -------------------------------------------------------------
+            % Create placeholder label in footer
+            % -------------------------------------------------------------
+            Obj.Components.FooterPlaceholder = uilabel(Obj.FooterGridLayout, ...
+                'BackgroundColor', [0.9412, 0.9412, 0.9412], ...
+                'HorizontalAlignment', 'center', ...
+                'Text', 'Drop content here');
+            Obj.Components.FooterPlaceholder.Layout.Row = 1;
+            Obj.Components.FooterPlaceholder.Layout.Column = 1;
+        end
+        % =================================================================
+        function update(Obj)
+            try
+                % ---------------------------------------------------------
+                % Timer
+                if Obj.Verbose; Time = now; end %#ok<TNOW1>
+                % ---------------------------------------------------------
+                % Update panel tags with current page number
+                Obj.Panel.Tag = sprintf('ReportPage_%i_Panel', Obj.PageNum);
+                Obj.GridLayout.Tag = sprintf('ReportPage_%i_GridLayout', Obj.PageNum);
+                Obj.HeaderPanel.Tag = sprintf('ReportPage_%i_HeaderPanel', Obj.PageNum);
+                Obj.HeaderGridLayout.Tag = sprintf('ReportPage_%i_HeaderGridLayout', Obj.PageNum);
+                Obj.BodyPanel.Tag = sprintf('ReportPage_%i_BodyPanel', Obj.PageNum);
+                Obj.BodyGridLayout.Tag = sprintf('ReportPage_%i_BodyGridLayout', Obj.PageNum);
+                Obj.FooterPanel.Tag = sprintf('ReportPage_%i_FooterPanel', Obj.PageNum);
+                Obj.FooterGridLayout.Tag = sprintf('ReportPage_%i_FooterGridLayout', Obj.PageNum);
+                % ---------------------------------------------------------
+                if Obj.Verbose
+                    fprintf('>> CIC: ReportPage %i updated in %.1g s.\n', Obj.PageNum, (now-Time)*24*60*60) %#ok<TNOW1>
+                end
+            catch ME
+                printerrormessage(ME, 'The error occurred during ''update'' in ReportPage.m')
+            end
+        end
+    end
+    % *********************************************************************
+    methods (Access = public)
+        function hUpdate(Obj, app, event) %#ok<INUSD>
+            try
+                % ---------------------------------------------------------
+                % This method will be called when events are broadcast
+                % Can be extended to handle eStyleChanged, eLogoChanged, etc.
+                % ---------------------------------------------------------
+            catch ME
+                printerrormessage(ME, 'The error occurred during ''hUpdate'' in ReportPage.m')
+            end
+        end
+    end
+end
---
+++ package/Components/Reports/ReportTab.m
@@ -0,0 +1,118 @@
+% REPORTTAB
+% The content of the tab that displays report pages
+
+% Authors:
+%   Rick Wassing, Woolcock Institute of Medical Research, Sydney, Australia
+%
+% History:
+%   Created 2025-11-11, Rick Wassing
+
+% Cicada (C) 2023 by Rick Wassing is licensed under
+% Attribution-NonCommercial-ShareAlike 4.0 International
+% This license requires that reusers give credit to the creator. It allows
+% reusers to distribute, remix, adapt, and build upon the material in any
+% medium or format, for noncommercial purposes only. If others modify or
+% adapt the material, they must license the modified material under
+% identical terms.
+
+classdef ReportTab < CicadaComponentContainer
+    % *********************************************************************
+    % PROPERTIES
+    properties
+        NumPages = 1;
+        PageHeight = 842;  % A4 height at 72 DPI
+        PageWidth = 595;   % A4 width at 72 DPI
+    end
+    properties (Access = public, Transient, NonCopyable)
+        Panel matlab.ui.container.Panel
+        GridLayout matlab.ui.container.GridLayout
+        Pages ReportPage
+    end
+    % *********************************************************************
+    % METHODS
+    methods (Access = protected)
+        % =================================================================
+        function setup(Obj)
+            % -------------------------------------------------------------
+            % Create sub-components
+            % -------------------------------------------------------------
+            Colors = app_colors();
+            Obj.Tag = 'ReportTab';
+            Obj.Panel = uipanel(Obj, ...
+                'Tag', 'ReportTab_Panel', ...
+                'Units', 'normalized', ...
+                'BorderType', 'none', ...
+                'Position', [0, 0, 1, 1]);
+            % -------------------------------------------------------------
+            Obj.GridLayout = uigridlayout(Obj.Panel, ...
+                'Tag', 'ReportTab_GridLayout', ...
+                'ColumnWidth', {'1x', 595, '1x'}, ...
+                'RowHeight', {842}, ...
+                'ColumnSpacing', 0, ...
+                'RowSpacing', 24, ...
+                'Padding', [0, 24, 0, 24], ...
+                'Scrollable', 'on', ...
+                'BackgroundColor', Colors.bg_secondary);
+        end
+        % =================================================================
+        function update(Obj)
+            try
+                % ---------------------------------------------------------
+                % Timer
+                if Obj.Verbose; Time = now; end %#ok<TNOW1>
+                % ---------------------------------------------------------
+                Obj.GridLayout.RowHeight = repmat({Obj.PageHeight}, 1, Obj.NumPages);
+                for i = 1:Obj.NumPages
+                    DoRender = false;
+                    if i > length(Obj.Pages)
+                        DoRender = true;
+                    elseif ~isvalid(Obj.Pages(i))
+                        DoRender = true;
+                    end
+                    if DoRender
+                        Obj.Pages(i) = ReportPage(Obj.GridLayout);
+                    else
+                        Obj.Pages(i).Parent = Obj.GridLayout;
+                    end
+                    Obj.Pages(i).PageNum = i;
+                    Obj.Pages(i).Verbose = Obj.Verbose;
+                    Obj.Pages(i).Layout.Column = 2;
+                    Obj.Pages(i).Layout.Row = i;
+                end
+                for i = length(Obj.Pages):-1:Obj.NumPages+1
+                    delete(Obj.Pages(i));
+                    Obj.Pages(i) = [];
+                end
+                % ---------------------------------------------------------
+                if Obj.Verbose
+                    fprintf('>> CIC: ReportTab updated ''%i'' pages in %.1g s.\n', Obj.NumPages, (now-Time)*24*60*60) %#ok<TNOW1>
+                end
+            catch ME
+                printerrormessage(ME, 'The error occurred during ''update'' in ReportTab.m')
+            end
+        end
+    end
+    % *********************************************************************
+    methods (Access = public)
+        function hUpdate(Obj, app, event) %#ok<INUSD>
+            try
+                % ---------------------------------------------------------
+                if isempty(app.ACT)
+                    % Set to default values
+                    Obj.NumPages = 0;
+                else
+                    % Get number of pages from settings or default to 1
+                    if isfield(app.Props, 'Settings') && ...
+                       isfield(app.Props.Settings, 'Reports') && ...
+                       isfield(app.Props.Settings.Reports, 'NumPages')
+                        Obj.NumPages = app.Props.Settings.Reports.NumPages;
+                    else
+                        Obj.NumPages = 1;
+                    end
+                end
+            catch ME
+                printerrormessage(ME, 'The error occurred during ''hUpdate'' in ReportTab.m')
+            end
+        end
+    end
+end
